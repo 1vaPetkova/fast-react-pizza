ad67303490443044d079b0bdc3762be77781b30b
Iva Petkova <iva1petkova@gmail.com>
2024-01-19 19:40:34 +0200

    add more styling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Form, redirect, useNavigation, useActionData } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
@@ -41,33 +40,50 @@ function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
+  const inputStyle = `mb-5 gap-2 flex flex-col 
+  sm:flex-row sm:items-center`;
+
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST" action="/order/new">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className={inputStyle}>
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" required className="input grow" />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
+        <div className={inputStyle}>
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input
+              type="tel"
+              name="phone"
+              required
+              className="input md:w-full"
+            />
+            {formErrors?.phone && (
+              <p className="text-s mt-2 rounded-full bg-red-100 p-2 text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className={inputStyle}>
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input md:w-full"
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             type="checkbox"
             name="priority"
@@ -78,12 +94,16 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button>{isSubmitting ? 'Placing Order...' : 'Order now'}</Button>
+          <Button type="primary">
+            {isSubmitting ? 'Placing Order...' : 'Order now'}
+          </Button>
         </div>
       </Form>
     </div>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,12 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="flex items-center justify-around border-b-4 border-stone-400 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header
+      className="text-pizza flex items-center justify-around
+     border-b-4
+     border-stone-400 bg-yellow-500 px-4 py-3 uppercase
+     sm:px-6"
+    >
       <Link to="/" className="tracking-[5px]">
         Fast React Pizza Co.
       </Link>
